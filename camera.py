b5ea61e7599e86578359f8852b3e30105b174362
tumulranjan1 <54700350+tumulranjan1@users.noreply.github.com>
2022-05-30 07:06:04 +0530

    Add files via upload

---
+++ camera.py
@@ -0,0 +1,30 @@
+import cv2
+from model import WSModel
+import numpy as np
+
+model = WSModel("model.json", "model_weights.h5")
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+class VideoCamera(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+
+    def __del__(self):
+        self.video.release()
+
+    # returns camera frames along with bounding boxes and predictions
+    def get_frame(self):
+        _, fr = self.video.read()
+        gray_fr = cv2.cvtColor(fr, cv2.COLOR_BGR2GRAY)
+
+        for (x, y, w, h) in faces:
+            fc = gray_fr[y:y+h, x:x+w]
+
+            roi = cv2.resize(fc, (48, 48))
+            pred = model.predict_emotion(roi[np.newaxis, :, :, np.newaxis])
+
+            cv2.putText(fr, pred, (x, y), font, 1, (255, 255, 0), 2)
+            cv2.rectangle(fr,(x,y),(x+w,y+h),(255,0,0),2)
+
+        _, jpeg = cv2.imencode('.jpg', fr)
+        return jpeg.tobytes()
